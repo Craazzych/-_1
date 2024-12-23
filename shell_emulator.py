77c75e813805babcd7d6d2bdada3f23dbd820f94
Craazzych <150323528+Craazzych@users.noreply.github.com>
2024-12-23 14:10:59 +0300

    Update shell_emulator.py

--- shell_emulator.py
+++ shell_emulator.py
@@ -11,12 +11,22 @@ class ShellEmulator:
         self.command_handler = CommandHandler(self)
 
     def _load_virtual_fs(self, tar_file_path):
-        fs = {}
-        with tarfile.open(tar_file_path, 'r') as tar:
-            for member in tar.getmembers():
-                fs[member.name] = tar.extractfile(member).read().decode('utf-8') if member.isfile() else None
-        return fs
-
+    fs = {}
+    with tarfile.open(tar_file_path, 'r') as tar:
+        for member in tar.getmembers():
+            # даляем './' из имени файла
+            clean_name = member.name.lstrip('./')
+            if member.isfile():
+                file_obj = tar.extractfile(member)
+                try:
+                    content = file_obj.read().decode('utf-8') if file_obj else None
+                except UnicodeDecodeError:
+                    content = None  
+                fs[clean_name] = content
+            else:
+                fs[clean_name] = None  
+    return fs
+    
     def execute_command(self, command):
         return self.command_handler.execute(command)
 
